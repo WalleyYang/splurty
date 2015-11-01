8381b9a5e0f6c7031034928bf0137c37cd68a706
Walley Yang <walleyyang@gmail.com>
2015-11-01 18:18:50 +0000

    Added test for quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,22 +1,29 @@
 class QuotesController < ApplicationController
 	def index
-	  @quote = Quote.order("RANDOM()").first
+		@quote = Quote.order("RANDOM()").first
 	end
 
 	def create
-	  @quote = Quote.create(quote_params)
-	  if @quote.invalid?
-	  	flash[:error] = '<strong>Could not save</strong> the data you entered is invalid'
-	  end
-	  redirect_to root_path
+		@quote = Quote.create(quote_params)
+		if @quote.invalid?
+			flash[:error] = '<strong>Could not save</strong> the data you entered is invalid'
+		end
+		redirect_to root_path
 	end
 
 	def about
 	end
 
+	def show
+		@quote = Quote.where(:id => params[:id]).first
+		if @quote.blank?
+			render :text => "Not Found", :status => :not_found
+		end
+	end
+
 	private
 
 	def quote_params
-	  params.require(:quote).permit(:saying, :author)
+		params.require(:quote).permit(:saying, :author)
 	end
 end
